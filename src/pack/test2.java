61d8741ae9928b390ea2916bd0bc83b60abc0b1c
shabana <xyz@gmail.com>
2022-12-04 17:14:17 +0530

    commiting test2  to branchS

---
+++ src/pack/test2.java
@@ -0,0 +1,14 @@
+package pack;
+
+public class test2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+
+		System.out.println("hello shabana.c");
+	
+
+	}
+
+}
